9fa797d1a0ce5eb1905ba5d2a068c69b04ea9486
Nikhitha-Anumandla <nikhitha472000@gmail.com>
2022-04-09 22:57:47 +0530

    Initial Solution setup

---
+++ ProCart.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProCart.WebUI.MvcApplication" Language="C#" %>
---
+++ ProCart.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProCart.WebUI.Startup))]
+namespace ProCart.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
