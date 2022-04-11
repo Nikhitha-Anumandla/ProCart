c439d6a085c879973ac84fbad9a4a589de793370
Nikhitha-Anumandla <nikhitha472000@gmail.com>
2022-04-11 16:21:52 +0530

    Added entity framework

---
+++ ProCart.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using ProCart.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCart.DataAccess.SQL
+{
+    public class DataContext:DbContext
+    {
+        public DataContext()
+            :base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Products> Products { get; set; }
+        public DbSet<ProductCategories> ProductCategories { get; set; }
+
+    }
+}
---
+++ ProCart.DataAccess.SQL/Migrations/202204111048571_Initial.cs
@@ -0,0 +1,42 @@
+﻿namespace ProCart.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProductCategories",
+                c => new
+                    {
+                        id = c.String(nullable: false, maxLength: 128),
+                        categories = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 20),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Description = c.String(),
+                        Image = c.String(),
+                        Category = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+            DropTable("dbo.ProductCategories");
+        }
+    }
+}
